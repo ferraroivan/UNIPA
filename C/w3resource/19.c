22ab39dcdf151942304f053a5038a10269246f4f
Ivan Ferraro <ivan@Mac-mini.fritz.box>
2023-08-07 22:52:39 +0200

    exericse 20 in C

--- C/w3resource/19.c
+++ C/w3resource/19.c
@@ -5,5 +5,22 @@
 
 int main(int argc, char* argv[]) {
 
+  int p, q, r, s;
+
+  printf("Input the first integer: ");
+  scanf("%d", &p);
+  printf("Input the second integer: ");
+  scanf("%d", &q);
+  printf("Input the third integer: ");
+  scanf("%d", &r);
+  printf("Input the fourth integer: ");
+  scanf("%d", &s);
+
+  if (q > r && s > p && r+s > p+q && r > 0 && s > 0 && p%2==0) {
+    printf("Correct values");
+  } else {
+    printf("Wrong values");
+  }
+
   return 0;
 }
